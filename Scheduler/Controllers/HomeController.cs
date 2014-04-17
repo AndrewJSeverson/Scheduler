7eeece5b8b30410176f4ef82ae8690e9f210fabb
sbesh91 <s.beshensky@hotmail.com>
2014-04-17 11:16:10 -0500

    stuff

--- Scheduler/Controllers/HomeController.cs
+++ Scheduler/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Scheduler.Models;
 
 namespace Scheduler.Controllers
 {
@@ -15,6 +16,28 @@ namespace Scheduler.Controllers
         {
             return View();
         }
-
+        public static List<processItem> getProcessData(int numProc)
+        {
+            var temp = new List<processItem>();
+            for (var i = 0; i < numProc; i++)
+            {
+                var name = "P" + (i+1);
+                var random = new Random();
+                var randomNumber = random.Next(1, 4);
+                var processItem = new processItem
+                {
+                    name = name,
+                    BurstArray = new int[randomNumber * 2 + 1],
+                    arrivalTime = random.Next(1,12);
+                };
+                for (var j = 0; j < processItem.BurstArray.Length; j++)
+                {
+                    var randTime = random.Next(1, 20);
+                    processItem.BurstArray[j] = randTime;
+                }
+                temp.Add(processItem);
+            }
+            return temp;
+        }
     }
 }
---
+++ Scheduler/Models/processItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler.Models
+{
+    public class processItem
+    {
+        public string name { get; set; }
+        public int[] BurstArray { get; set; }
+        public int arrivalTime { get; set; }
+    }
+}
