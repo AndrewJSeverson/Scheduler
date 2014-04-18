27a0c20086ded68487c159e2e9348c68db678717
barthk63 <barthk63@uwosh.edu>
2014-04-17 23:00:59 -0500

    adding base classes

---
+++ Scheduler/Classes/Scheduler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Scheduler.Models;
+
+namespace Scheduler.Classes
+{
+    public abstract class Scheduler
+    {
+        public SchedulerStats SchedulerStats { get; set; }
+
+        public abstract List<Process> Run();
+    }
+}
---
+++ Scheduler/Models/SchedulerStats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler.Models
+{
+    public class SchedulerStats
+    {
+        public decimal CpuUtilization { get; set; }
+
+        public decimal WaitingTime { get; set; }
+
+        public decimal AverageWaitingTime { get; set; }
+
+        public decimal MeanTurnAroundTime { get; set; }
+
+        public decimal NormailizedTurnAroundTime { get; set; }
+    }
+}
